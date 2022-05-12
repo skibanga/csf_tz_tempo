e464383c79d65419c0fb0e69d32f88b1477d7911
eliusmgani <eliasmgani@gmail.com>
2022-05-12 09:27:28 +0000

    fix: 'NoneType' object is not iterable, as tira were not able to recognize some of the registration number

--- csf_tz/csf_tz/doctype/tz_insurance_cover_note/tz_insurance_cover_note.py
+++ csf_tz/csf_tz/doctype/tz_insurance_cover_note/tz_insurance_cover_note.py
@@ -11,43 +11,49 @@ class TZInsuranceCoverNote(Document):
 
 @frappe.whitelist()
 def update_covernote_docs():
-	"""Create or Update covernote document after getting necessary details from tira"""
-	
+	"""Create or Update covernote document after getting necessary details from tira
+
+	The routine to create or update covernote document runs on 00:00 am, 1st date of every month
+	Forexample:
+		for June: a routine will run on 00:00 am, June 1, 2022
+	"""
+
 	motor_vehicles = frappe.get_all('Vehicle', 'name')
 	
 	for vehicle in motor_vehicles:
 		req = get_covernote_details(vehicle.name)
-		if not req:
-			continue
+		try:
+			for record in req.get('data'):
+				if not frappe.db.exists('TZ Insurance Cover Note', record['coverNoteNumber']):
+					doc = frappe.new_doc('TZ Insurance Cover Note')
+				else:
+					doc = frappe.get_doc('TZ Insurance Cover Note', record['coverNoteNumber'])
+				
+				for key, value in record.items():
+					
+					if key.lower() == 'motor':
+						doc.insurance_motors = []
+						doc.append('insurance_motors', {h.lower(): v for  h, v in value.items()})
+					
+					if key.lower() == 'company':
+						doc.insurance_provider = []
+						doc.append('insurance_provider', {
+							h.lower(): json.dumps(v) if h == 'shareholders' else v for  h, v in value.items()
+						})
+					
+					if key.lower() == 'policyholders':
+						doc.policy_holders = []
+						for i, row in enumerate(value):
+							doc.append('policy_holders', {h.lower(): v for  h, v in row.items()})
+					
+					if key.lower() not in ['company', 'motor', 'policyholders']:
+						doc.update({key.lower(): value})
+					
+				doc.vehicle = vehicle.name
+				doc.save(ignore_permissions=True)
 		
-		for record in req.get('data'):
-			if not frappe.db.exists('TZ Insurance Cover Note', record['coverNoteNumber']):
-				doc = frappe.new_doc('TZ Insurance Cover Note')
-			else:
-				doc = frappe.get_doc('TZ Insurance Cover Note', record['coverNoteNumber'])
-			
-			for key, value in record.items():
-				
-				if key.lower() == 'motor':
-					doc.insurance_motors = []
-					doc.append('insurance_motors', {h.lower(): v for  h, v in value.items()})
-				
-				if key.lower() == 'company':
-					doc.insurance_provider = []
-					doc.append('insurance_provider', {
-						h.lower(): json.dumps(v) if h == 'shareholders' else v for  h, v in value.items()
-					})
-				
-				if key.lower() == 'policyholders':
-					doc.policy_holders = []
-					for i, row in enumerate(value):
-						doc.append('policy_holders', {h.lower(): v for  h, v in row.items()})
-				
-				if key.lower() not in ['company', 'motor', 'policyholders']:
-					doc.update({key.lower(): value})
-				
-			doc.vehicle = vehicle.name
-			doc.save(ignore_permissions=True)
+		except Exception as e:
+			frappe.log_error(frappe.get_traceback(), str(e))
 	
 	frappe.db.commit()
 	return True
@@ -73,7 +79,7 @@ def get_covernote_details(regnumber):
 		if response.status_code == 200:
 			return json.loads(response.text)
 		else:
-			frappe.log_error(response.text)
+			frappe.log_error(response.text, str(response.status_code))
 
 	except Exception as e:
 		frappe.log_error(frappe.get_traceback(), str(e))
