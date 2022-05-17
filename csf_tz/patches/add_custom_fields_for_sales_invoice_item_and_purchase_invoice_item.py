c53937132da9ab1fd0a89844c2dfa976a3571749
eliusmgani <eliasmgani@gmail.com>
2022-05-17 13:07:14 +0000

    fix: typing error

--- csf_tz/patches/add_custom_fields_for_sales_invoice_item_and_purchase_invoice_item.py
+++ csf_tz/patches/add_custom_fields_for_sales_invoice_item_and_purchase_invoice_item.py
@@ -8,7 +8,7 @@ def execute():
             dict(fieldname='csf_tz_wtax_jv_created', label='CSF TZ Wtax JV Created', fieldtype='Check', insert_after='csf_tz_specifics',
             read_only=1, allow_on_submit=1),
             dict(fieldname='csf_tz_wtax_column_break', label='', fieldtype='Column Break', insert_after='csf_tz_wtax_jv_created'),
-            dict(fieldname='csf_tz_create_wtax_entry', label='Create WTax Entry', fieldtype='Buttom', insert_after='csf_tz_wtax_column_break',
+            dict(fieldname='csf_tz_create_wtax_entry', label='Create WTax Entry', fieldtype='Button', insert_after='csf_tz_wtax_column_break',
             depends_on='eval: doc.docstatus == 1 && doc.withholding_tax_rate > 0 && doc.csf_tz_wtax_jv_created == 0')
         ],
         'Sales Invoice Item': [
