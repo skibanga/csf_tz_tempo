55e85b21637cab2dea9f1ade8d94258e9211c40f
Aakvatech Limited <info@aakvatech.com>
2022-11-10 05:08:41 +0100

    feat: Added Bank Charges Pattern for Bank Transaction

---
+++ csf_tz/csf_tz/doctype/bank_charges_pattern/bank_charges_pattern.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bank Charges Pattern', {
+	// refresh: function(frm) {
+
+	// }
+});
