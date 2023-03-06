e74bf414be1d53188438b9da21df6a6e0f9e9645
aakvatech <info@aakvatech.com>
2023-03-06 12:54:14 +0000

    update: OTP Register doctype

--- csf_tz/authotp/doctype/otp_register/otp_register.js
+++ csf_tz/authotp/doctype/otp_register/otp_register.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('OTP Register', {
-	// refresh: function(frm) {
+	refresh: function (frm) {
+		// set filter for on party_type field
+		frm.set_query("party_type", function () {
+			return {
+				filters: {
+					"name": ["in", ["Customer", "Supplier", "Employee", "User", "Lead", "Student", "Guardian", "Instructor", "Member", "Sales Partner", "Sales Person", "Teacher", "Tenant", "Visitor", "Patient"]]
+				}
+			};
+		});
 
-	// }
+	},
+	party_type: function (frm) {
+		// clear party, party_name and user_name fields
+		frm.set_value("party", "");
+		frm.set_value("party_name", "");
+		frm.set_value("user_name", "");
+	},
 });
--- csf_tz/authotp/doctype/otp_register/otp_register.py
+++ csf_tz/authotp/doctype/otp_register/otp_register.py
@@ -1,8 +1,83 @@
 # Copyright (c) 2023, Aakvatech and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class OTPRegister(Document):
-	pass
+    def validate(self):
+        self.set_party_name()
+
+    def set_party_name(self):
+        if not self.party:
+            return
+        if self.party_type == "Customer":
+            self.party_name = frappe.get_cached_value(
+                "Customer", self.party, "customer_name"
+            )
+        elif self.party_type == "Supplier":
+            self.party_name = frappe.get_cached_value(
+                "Supplier", self.party, "supplier_name"
+            )
+        elif self.party_type == "Employee":
+            self.party_name = frappe.get_cached_value(
+                "Employee", self.party, "employee_name"
+            )
+        elif self.party_type == "User":
+            self.party_name = frappe.get_cached_value("User", self.party, "full_name")
+        elif self.party_type == "Contact":
+            self.party_name = frappe.get_cached_value(
+                "Contact", self.party, "first_name"
+            )
+        elif self.party_type == "Lead":
+            self.party_name = frappe.get_cached_value("Lead", self.party, "lead_name")
+        elif self.party_type == "Sales Partner":
+            self.party_name = self.party
+        elif self.party_type == "Student":
+            first_name, middle_name, last_name = frappe.get_cached_value(
+                "Student", self.party, ["first_name", "middle_name", "last_name"]
+            )
+            self.party_name = " ".join(
+                filter(None, [first_name, middle_name, last_name])
+            )
+        elif self.party_type == "Guardian":
+            self.party_name = frappe.get_cached_value(
+                "Guardian", self.party, "guardian_name"
+            )
+        elif self.party_type == "Donor":
+            self.party_name = frappe.get_cached_value("Donor", self.party, "donor_name")
+        elif self.party_type == "Member":
+            self.party_name = frappe.get_cached_value(
+                "Member", self.party, "member_name"
+            )
+        elif self.party_type == "Patient":
+            self.party_name = frappe.get_cached_value(
+                "Patient", self.party, "patient_name"
+            )
+        elif self.party_type == "Instructor":
+            self.party_name = frappe.get_cached_value(
+                "Instructor", self.party, "instructor_name"
+            )
+        elif self.party_type == "Sales Person":
+            self.party_name = frappe.get_cached_value(
+                "Sales Person", self.party, "sales_person_name"
+            )
+        elif self.party_type == "Teacher":
+            self.party_name = frappe.get_cached_value(
+                "Teacher", self.party, "teacher_name"
+            )
+        elif self.party_type == "Tenant":
+            self.party_name = frappe.get_cached_value(
+                "Tenant", self.party, "tenant_name"
+            )
+        elif self.party_type == "Visitor":
+            self.party_name = frappe.get_cached_value(
+                "Visitor", self.party, "visitor_name"
+            )
+
+        if not self.party_name:
+            self.party_name = self.party
+
+        if not self.user_name:
+            self.user_name = self.party_name
