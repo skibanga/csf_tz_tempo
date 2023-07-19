d4d579dc7e040ea6c320536302fde749de03828d
aakvatech <info@aakvatech.com>
2023-07-19 12:47:01 +0000

    fix: sales invoice-autho'tp_validated' is missing

--- csf_tz/patches/custom_fields/auth_otp_custom_fields.py
+++ csf_tz/patches/custom_fields/auth_otp_custom_fields.py
@@ -1,54 +1,56 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
+
 def execute():
-    fields={
+    fields = {
         "Customer": [
-            {
-                "fieldname": "default_authotp_method",
-                "fieldtype": "Link",
-                "insert_after": "is_authotp_applied",
-                "label": "Default AuthOTP Method",
-                "name": "Customer-default_authotp_method",
-                "options": "OTP Register"
-            },
             {
                 "fieldname": "is_authotp_applied",
                 "fieldtype": "Check",
                 "insert_after": "customer_group",
                 "label": "Is AuthOTP Applied",
-                "name": "Customer-is_authotp_applied",
+            },
+            {
+                "fieldname": "default_authotp_method",
+                "fieldtype": "Link",
+                "insert_after": "is_authotp_applied",
+                "label": "Default AuthOTP Method",
+                "options": "OTP Register",
             },
             {
                 "fieldname": "authotp_validated",
                 "fieldtype": "Check",
-                "insert_after": "column_break_sn02w",
+                "insert_after": "default_authotp_method",
                 "label": "AuthOTP Validated",
-                "name": "Sales Invoice-authotp_validated"
             },
         ],
-        "Sales Invoice":[
-            {
-                "fieldname": "column_break_sn02w",
-                "fieldtype": "Column Break",
-                "insert_after": "authotp_method",
-                "name": "Sales Invoice-column_break_sn02w"
-            },
-            {
-                "fieldname": "authotp_method",
-                "fieldtype": "Link",
-                "insert_after": "authotp_method",
-                "name": "Sales Invoice-authotp_method",
-                "options": "OTP Register",
-                "label": "AuthOTP Method"
-            },
+        "Sales Invoice": [
             {
                 "fieldname": "authotp",
                 "fieldtype": "Section Break",
                 "insert_after": "amended_from",
-                "name": "Sales Invoice-authotp",
-                "label": "AuthOTP"
+                "label": "AuthOTP",
             },
-        ]
+            {
+                "fieldname": "authotp_method",
+                "fieldtype": "Link",
+                "insert_after": "authotp",
+                "options": "OTP Register",
+                "label": "AuthOTP Method",
+            },
+            {
+                "fieldname": "column_break_sn02w",
+                "fieldtype": "Column Break",
+                "insert_after": "authotp_method",
+            },
+            {
+                "label": "AuthOTP Validated",
+                "fieldname": "authotp_validated",
+                "insert_after": "column_break_sn02w",
+                "fieldtype": "Check",
+                "read_only": 1,
+            },
+        ],
     }
     create_custom_fields(fields, update=True)
