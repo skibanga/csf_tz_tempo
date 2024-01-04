6fe03db60f9eb4402a06b62e34fa419806ada103
iphenelist <innocentphenelist@gmail.com>
2024-01-04 13:52:09 +0300

    feat: filter warehouses based on the company

--- csf_tz/csf_tz/doctype/inter_company_stock_transfer/inter_company_stock_transfer.js
+++ csf_tz/csf_tz/doctype/inter_company_stock_transfer/inter_company_stock_transfer.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inter Company Stock Transfer', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function (frm) {
+        frm.set_query('default_from_warehouse', () => {
+            return {
+                filters: {
+                    company: frm.doc.from_company
+                }
+            };
+        });
+        frm.set_query('default_to_warehouse', () => {
+            return {
+                filters: {
+                    company: frm.doc.to_company
+                }
+            };
+        });
+    }
 });
+
