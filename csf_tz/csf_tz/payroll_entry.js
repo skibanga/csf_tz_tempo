fe15f101cecbb6b59aa4a423c14c91e11fdc5265
aakvatech <35020381+aakvatech@users.noreply.github.com>
2024-02-13 15:47:49 +0300

    fix: Stanbic js broke csftz js for payroll entry

--- csf_tz/csf_tz/payroll_entry.js
+++ csf_tz/csf_tz/payroll_entry.js
@@ -1,3 +1,4 @@
+const buttonName = "Generate Stanbic Payments Initiation";
 frappe.ui.form.on("Payroll Entry", {
     setup: function(frm) {
         frm.trigger("control_action_buttons");
@@ -5,6 +6,11 @@ frappe.ui.form.on("Payroll Entry", {
     },
     refresh:function(frm) {
         frm.trigger("control_action_buttons");
+        if (frm.doc.docstatus == 1) generate_payments_initiation(frm);
+        else {
+          frm.remove_custom_button(__(buttonName));
+        }
+  },
     },
     onload: (frm) => {
         frm.trigger("control_action_buttons");
@@ -93,3 +99,46 @@ frappe.ui.form.on("Payroll Entry", {
         }
     },
 });
+
+function generate_payments_initiation(frm) {
+  let condition = true;
+  frappe.db
+    .get_list("Salary Slip", {
+      fields: ["name", "docstatus"],
+      filters: {
+        payroll_entry: frm.doc.name,
+      },
+    })
+    .then((res) => {
+      if (res.length == 0) condition = false;
+      // loop through the salary slips and check if all are submitted
+      res.forEach((slip) => {
+        if (slip.docstatus != 1) {
+          condition = false;
+        }
+      });
+      // if all salary slips are submitted, add custom button
+      if (condition == true) {
+        add_generate_payments_initiation_button(frm);
+      } else {
+        frm.remove_custom_button(__(buttonName));
+      }
+    });
+}
+
+function add_generate_payments_initiation_button(frm) {
+  frm.add_custom_button(__(buttonName), function () {
+    frappe.call({
+      method: "csf_tz.stanbic.payments.make_payments_initiation",
+      args: {
+        payroll_entry_name: frm.doc.name,
+        currency: frm.doc.currency,
+      },
+      callback: function (r) {
+        if (r.message) {
+          console.log(r.message);
+        }
+      },
+    });
+  });
+}
