9314dce24ba9e4acaad2be27108846a46d5f9715
aakvatech <info@aakvatech.com>
2024-03-06 22:00:33 +0300

    feat: Do not allow duplicate creation unless cheque number is cleared purposely and tracked.

--- csf_tz/stanbic/payments.py
+++ csf_tz/stanbic/payments.py
@@ -1,12 +1,21 @@
 import frappe
+from frappe.utils import get_date_str
+from frappe import _
 
 
 @frappe.whitelist()
 def make_payments_initiation(payroll_entry_name, currency, stanbic_setting_name=None):
     if currency and not stanbic_setting_name:
-        company = frappe.get_cached_value(
-            "Payroll Entry", payroll_entry_name, "company"
+        company, cheque_number = frappe.get_cached_value(
+            "Payroll Entry", payroll_entry_name, ["company", "cheque_number"]
         )
+        if cheque_number:
+            frappe.throw(
+                _(
+                    f"Payments initiation {cheque_number} already created for payroll entry {payroll_entry_name}. Please remove the cheque number and try again if you really want to create the payments initiation file."
+                ),
+                title="Payments initiation already created",
+            )
         stanbic_setting_doc = frappe.get_doc(
             "Stanbic Setting", {"company": company, "currency": currency}
         )
@@ -24,5 +33,17 @@ def make_payments_initiation(payroll_entry_name, currency, stanbic_setting_name=
     payments_initiation_doc.set_data()
     frappe.msgprint(
         _(f"Payments Initiation {payments_initiation_doc.name} created successfully.")
+    )
+    frappe.db.set_value(
+        "Payroll Entry",
+        payroll_entry_name,
+        "cheque_date",
+        payments_initiation_doc.posting_date,
+    )
+    frappe.db.set_value(
+        "Payroll Entry",
+        payroll_entry_name,
+        "cheque_number",
+        payments_initiation_doc.name,
     )
     return payments_initiation_doc
