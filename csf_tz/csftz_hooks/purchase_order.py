540575afb07d966c7cc28efdc9005de8b24e3e95
av-dev2 <av-dev2@aakvatech.com>
2024-08-13 19:37:13 +0300

    feat: validate reopen of PO based on role settled/specified on CSF TZ Settings

---
+++ csf_tz/csftz_hooks/purchase_order.py
@@ -0,0 +1,39 @@
+import frappe
+from frappe import _
+
+
+@frappe.whitelist()
+def update_po_status(status, name):
+    from erpnext.buying.doctype.purchase_order.purchase_order import update_status
+
+
+    if status != "Submitted":
+        update_status(status, name)
+        return
+
+    csf_tz_settings = frappe.get_doc("CSF TZ Settings")
+    if csf_tz_settings.allow_reopen_of_po_based_on_role == 1:
+        roles = frappe.get_roles()
+        if csf_tz_settings.role in roles:
+            update_status(status, name)
+        else:
+            frappe.throw(_(f"You are not allowed to reopen this Purchase Order: <b>{name}</b>"))
+
+
+@frappe.whitelist()
+def close_or_unclose_purchase_orders(names, status):
+    from erpnext.buying.doctype.purchase_order.purchase_order import close_or_unclose_purchase_orders
+
+    if status != "Submitted":
+        close_or_unclose_purchase_orders(names, status)
+        return
+    
+    csf_tz_settings = frappe.get_doc("CSF TZ Settings")
+    if csf_tz_settings.allow_reopen_of_po_based_on_role == 1:
+        roles = frappe.get_roles()
+        if csf_tz_settings.role in roles:
+            close_or_unclose_purchase_orders(names, status)
+        else:
+            frappe.throw(_("<b>You are not allowed to reopen Purchase Orders</b>"))
+
+
