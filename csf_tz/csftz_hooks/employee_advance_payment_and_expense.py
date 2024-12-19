b721fd2b32d5724b91940141eeeb4b6b8375ed9d
Sydney Kibanga <skibanga@aakvatech.com>
2024-12-19 17:09:33 +0300

    fix: Allocated amount must not exceed unclaimed amount

--- csf_tz/csftz_hooks/employee_advance_payment_and_expense.py
+++ csf_tz/csftz_hooks/employee_advance_payment_and_expense.py
@@ -16,9 +16,6 @@ def execute(doc, method):
         payment_entry = create_payment_entry(doc)
         doc.reload()
 
-        if payment_entry:
-            create_expense_claim(doc)
-
     except Exception as e:
         frappe.throw(f"Error during Employee Advance submission: {str(e)}")
 
@@ -38,67 +35,3 @@ def create_payment_entry(doc):
 
     except Exception as e:
         frappe.throw(f"Error creating Payment Entry: {str(e)}")
-
-
-def create_expense_claim(doc):
-    try:
-
-        payable_account = frappe.get_cached_value(
-            "Company", doc.company, "default_expense_claim_payable_account"
-        )
-
-        if not payable_account:
-            frappe.throw(
-                f"Expense claim payable account is not set for the company {doc.company}"
-            )
-
-        costings = []
-        travel_request = frappe.get_doc("Travel Request", doc.travel_request_ref)
-        company_abbr = frappe.get_cached_value("Company", doc.company, "abbr")
-        cost_center = travel_request.cost_center or f"Main - {company_abbr}"
-        costings = [
-            {
-                "expense_type": costing.expense_type,
-                "amount": costing.total_amount,
-                "sanctioned_amount": costing.total_amount,
-                "cost_center": cost_center,
-            }
-            for costing in travel_request.costings
-        ]
-
-        if not costings:
-            frappe.throw(
-                "No expenses available for this claim. Please add costing details to the Travel Request."
-            )
-
-        expense_claim = frappe.get_doc(
-            {
-                "doctype": "Expense Claim",
-                "employee": doc.employee,
-                "employee_name": doc.employee_name,
-                "posting_date": frappe.utils.nowdate(),
-                "total_claimed_amount": doc.advance_amount,
-                "company": doc.company,
-                "payable_account": payable_account,
-                "expenses": costings,
-                "approval_status": "Approved",
-                "advances": [
-                    {
-                        "employee_advance": doc.name,
-                        "advance_paid": doc.advance_amount,
-                        "advance_account": doc.advance_account,
-                        "allocated_amount": doc.advance_amount,
-                    }
-                ],
-            }
-        )
-
-        # Insert and submit the expense claim
-        expense_claim.insert(ignore_permissions=True)
-        # expense_claim.submit()
-        doc.reload()
-
-        frappe.msgprint(f"Expense Claim {expense_claim.name} created successfully.")
-
-    except Exception as e:
-        frappe.throw(f"Error creating Expense Claim: {str(e)}")
