acfb418c9418b16e0a3a64d5e6511085450e17dc
aakvatech <35020381+aakvatech@users.noreply.github.com>
2026-09-13 19:53:29 +0300

    refactor(jobcards): load Vuetify 3 styles through bundle

--- csf_tz/public/js/jobcards/jobcards.bundle.js
+++ csf_tz/public/js/jobcards/jobcards.bundle.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
-import { createVuetify } from 'vuetify';
-import JobCardsComponent from './JobCards.vue';
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import JobCardsComponent from "./JobCards.vue";
 
 class JobCardsBuilder {
     constructor({ wrapper, page }) {
@@ -8,22 +9,15 @@ class JobCardsBuilder {
         this.page = page;
         this.init();
     }
-    
+
     init() {
-        // Create Vuetify instance
         const vuetify = createVuetify();
-        
-        // Create Vue app
         const app = createApp(JobCardsComponent);
-        
-        // Use Vuetify
+
         app.use(vuetify);
-        
-        // Mount the app
         this.vue = app.mount(this.$wrapper[0]);
     }
 }
 
-// Export the class
-frappe.provide('frappe.JobCards');
+frappe.provide("frappe.JobCards");
 frappe.JobCards.JobCardsBuilder = JobCardsBuilder;
